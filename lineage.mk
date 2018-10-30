59fa8c7f9f31e8f5e40c9ab42cf8fad84afcb37c
Samad SegMane <32767790+MrSaMaDo@users.noreply.github.com>
2018-10-30 09:43:41 -0700

    X5: Updates

--- lineage.mk
+++ lineage.mk
@@ -4,8 +4,11 @@ PRODUCT_RELEASE_NAME := X5
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
 # Inherit device configuration
-$(call inherit-product, device/doogee/x5/lineage_x5.mk)
+$(call inherit-product, device/doogee/x5/device_x5.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x5
@@ -18,5 +21,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=DOOGEE/full_hct6580_weg_c_m/hct6580_weg_c_m:6.0/MRA58K/1504516469:user/test-keys \
     PRIVATE_BUILD_DESC="full_hct6580_weg_c_m-user 6.0 MRA58K 1504516469 test-keys"
 
-#SuperUser
-WITH_SU := true
+# Get the long list of APNs
+PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml
+
+# SuperUser
+WITH_SU := false
+WITH_ROOT := false
