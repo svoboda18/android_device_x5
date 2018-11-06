1b72e91f5d13705107828c6cd58fb427bc470d69
svoboda18 <ruspam18@gmail.com>
2018-11-06 18:49:41 +0100

    X5: Fixes * Fix Battery Voltage,In Order To Get Longer Life.

--- patches/do.sh
+++ patches/do.sh
@@ -14,31 +14,26 @@ patch -p1 < ../../device/doogee/x5/patches/0004-libnetutils-add-MTK-bits-ifc_ccm
 patch -p1 < ../../device/doogee/x5/patches/0012-PATCH-xen0n-some-MTK-services-e.g.-ril-daemon-mtk-re.patch
 cd ../..
 cd bionic
-
 patch -p1 < ../device/doogee/x5/patches/0002-Apply-LIBC-version-to-__pthread_gettid.patch
 cd ..
 cd system/sepolicy
-
 patch -p1 < ../../device/doogee/x5/patches/0001-system_sepolicy.patch
 cd ../..
 cd frameworks/av
-
 patch -p1 < ../../device/doogee/x5/patches/0001-frameworks_av.patch
 cd ../..
 cd frameworks/native
-
 patch -p1 < ../../device/doogee/x5/patches/frameworks_native.patch
 cd ../..
 cd frameworks/base
 patch -p1 < ../../device/doogee/x5/patches/0011-Fix-Minior-Bugs.patch
 patch -p1 < ../../device/doogee/x5/patches/0007-fix-out-of-memory-gl-crashes.patch
+patch -p1 < ../../device/doogee/x5/patches/0014-MTK-Use-Kernel-Based-Voltage-Value.patch
 cd ../..
 cd system/netd
-
 patch -p1 < ../../device/doogee/x5/patches/0010-wifi-tethering-fix.patch
 cd ../..
 cd external/wpa_supplicant_8
-
 patch -p1 < ../../device/doogee/x5/patches/0001-Ignore-fake-nvram-wifi.patch
 patch -p1 < ../../device/doogee/x5/patches/0005-fix-wlan-hidden-wifi-scan.patch
 cd ../..
